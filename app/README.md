94372d1f6381bfb2221f840852cc5bc461c5b3f8
jonathanp2004 <jpadilla1782.2@gmail.com>
2025-09-16 11:42:46 -0500

    Add ListView for pet display and create pet model class

---
+++ app/README.md
@@ -0,0 +1,8 @@
+Jonathan Padilla
+
+ activity_main.xml has a constraint Layout (found on line 2) with a single textView which reads "Pet Care" 
+ 
+The string resources in are strings.xml : <string name="app_name">ListViewExample</string>
+                                          <string name="petcare">PetCare</string>
+
+Code: Methods: populateListView() on line 18
---
+++ app/src/main/java/model/pet.java
@@ -0,0 +1,37 @@
+package model;
+
+public class pet {
+    String name;
+    String breed;
+    int age;
+
+    public pet(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
